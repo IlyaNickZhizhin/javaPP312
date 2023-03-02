e25c183e57fc18079672f86b64c3e2519f665db1
”Ilya <ilya.jjn@gmail.com>
2023-03-02 12:51:39 +0500

    edited according to comments #2 1 - DONE - import org.springframework.web.bind.annotation.* - импорты подгружаем только нужные, проверить настройки в идее, чтобы не меняло на звездочку (как настроить IDE не нашел искал в:         а) File" (Файл) -> "Settings" (Настройки) -> "Editor" (Редактор) -> "General" (Общее) -> "Auto Import" (Автоимпорт).         б) File" (Файл) -> "Settings" (Настройки) -> "Editor" (Редактор) -> "CodeStyle" -> "Java". 2 - DONE - @PatchMapping("/updateUser")            public String updateUser(@RequestParam("id") int id,                                                         Model model) {            User user = userService.getUserById(id);            model.addAttribute("user", user);            return "/user-info";           } - этот метод показывает юзера по ID, он должен быть GetMapping и должен называться типа showUserById. Не должно быть методов с одинаковым названием, которые делают разное. 3. - DONE -@RequestMapping(value = "/deleteUser")            public String deleteUser - сделать @DeleteMapping 4. - DONE -@PutMapping("/saveUser")- тут PatchMapping- частично обновляем юзера

--- src/main/java/pp/javapp312/controller/WebUserController.java
+++ src/main/java/pp/javapp312/controller/WebUserController.java
@@ -3,7 +3,12 @@ package pp.javapp312.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import pp.javapp312.model.User;
 import pp.javapp312.service.UserService;
 
@@ -19,12 +24,8 @@ public class WebUserController {
         this.userService = userService;
     }
 
-    @GetMapping("/")
-    public String index() {
-        return "index";
-    }
 
-    @GetMapping("/start")
+    @GetMapping("/")
     public String showAllUsers(Model model) {
         List<User> userList = userService.getAllUsers();
         model.addAttribute("users", userList);
@@ -34,26 +35,26 @@ public class WebUserController {
     @PostMapping(value = "/addNewUser")
     public String addNewUser(@ModelAttribute("newUser") User user) {
         userService.saveUser(user);
-        return "redirect:/start";
+        return "redirect:/";
     }
 
-    @PatchMapping("/updateUser")
-    public String updateUser(@RequestParam("id") int id,
+    @GetMapping("/showUserById")
+    public String showUserById(@RequestParam("id") int id,
                              Model model) {
         User user = userService.getUserById(id);
         model.addAttribute("user", user);
         return "/user-info";
     }
-    @PutMapping("/saveUser")
+    @PatchMapping("/saveUser")
     public String updateUser(@ModelAttribute("changedUser") User user) {
         userService.saveUser(user);
-        return "redirect:/start";
+        return "redirect:/";
     }
 
-    @RequestMapping(value = "/deleteUser")
+    @DeleteMapping(value = "/deleteUser")
     public String deleteUser(@RequestParam(name = "id") int id) {
         userService.deleteUser(id);
-        return "redirect:/start";
+        return "redirect:/";
     }
 
 }
